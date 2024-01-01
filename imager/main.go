790e3a209abb7eb5b4f5a5a71af561bc0c4ec8ca
Antonios Chariton (daknob) <daknob.mac@gmail.com>
2024-01-01 23:43:27 +0100

    Modernize imager
    * No longer fixed to /16
    * No longer bringing the entire input in memory
    * Switch to masscan -oL output format
    * Improved, more sophisticated parsing

--- imager/main.go
+++ imager/main.go
@@ -18,116 +18,147 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"image"
 	"image/color"
 	"image/png"
 	"log/slog"
+	"math"
+	"math/big"
+	"net/netip"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/google/hilbert"
 )
 
+const (
+	IPv4Bits    = 32
+	CommentChar = "#"
+)
+
 func main() {
 	slog.Info("starting imager...")
 
-	/* Accept the CSV file to image, default from STDIN */
+	/* Accept the file to image, default from STDIN */
 	inFile := flag.String("i", "/dev/stdin", "Input CSV File")
 
 	/* Accept the PNG file name to output as, default at STDOUT */
 	outFile := flag.String("o", "/dev/stdout", "Output PNG File")
+
+	/* Accept range to image, default to 193.5.16.0/22 */
+	imageRange := flag.String("r", "193.5.16.0/22", "Output range (CIDR must be even)")
 	flag.Parse()
 
-	/* Open the CSV file */
+	/* Open the file */
 	slog.Info("loading file", "name", *inFile)
-	cont, err := os.ReadFile(*inFile)
+	cont, err := os.Open(*inFile)
 	if err != nil {
-		slog.Error("could not read file", "name", *inFile, "error", err)
+		slog.Error("could not open file", "name", *inFile, "error", err)
 		os.Exit(1)
 	}
 	slog.Info("file loaded", "name", *inFile)
+
+	/* Parse the image range */
+	prefix, err := netip.ParsePrefix(*imageRange)
+	if err != nil {
+		slog.Error("failed to parse image range prefix", "error", err)
+		os.Exit(1)
+	}
+	if !prefix.Addr().Is4() {
+		slog.Error("imager does not support IPv6 at the moment :(")
+		os.Exit(2)
+	}
+	if prefix.Bits()%2 != 0 {
+		slog.Error("the image range CIDR must be even (e.g. /8, /10, /24, /30, ...)")
+		os.Exit(1)
+	}
+
+	/* Canonicalize prefix (e.g. 193.5.16.1/22 -> 193.5.16.0/22) */
+	prefix = prefix.Masked()
+
+	/* Calculate the range size */
+	rangeSize := int(math.Pow(2, float64(IPv4Bits-prefix.Bits())))
+
+	/* Calculate the range dimension */
+	rangeSqrt := int(math.Sqrt(float64(rangeSize)))
+
+	/* Calculate the first IP Address as a big.Int */
+	baseAs4 := prefix.Addr().As4()
+	rangeStart := big.NewInt(0).SetBytes(baseAs4[:])
+
+	slog.Info("prefix parsed", "base", prefix.Addr().String(), "length", prefix.Bits(), "size", rangeSize, "grid", rangeSqrt)
 
 	/* Create a new PNG image in memory */
 	slog.Info("creating image file...")
 	resultImage := image.NewRGBA(
 		image.Rectangle{
 			image.Point{0, 0},
-			image.Point{255, 255},
+			image.Point{rangeSqrt, rangeSqrt},
 		},
 	)
 
-	/* Create the output file, and open it for writting */
+	/* Fill image with offline color */
+	for i := 0; i < rangeSqrt; i++ {
+		for j := 0; j < rangeSqrt; j++ {
+			resultImage.Set(i, j, color.RGBA{50, 50, 50, 255})
+		}
+	}
+
+	/* Create the output file, and open it for writing */
 	resultFile, err := os.Create(*outFile)
 	if err != nil {
 		slog.Error("failed to create image file for writing", "name", *outFile, "error", err)
 		os.Exit(1)
 	}
 
-	/* Create a new Hilbert map, 256x256 to map the addresses in */
-	hilb, err := hilbert.NewHilbert(256)
+	/* Create a new Hilbert curve to map the addresses */
+	hilb, err := hilbert.NewHilbert(rangeSqrt)
 	if err != nil {
 		slog.Error("failed to create hilbert curve map", "error", err)
 		os.Exit(1)
 	}
 
-	/* Split the CSV file, line by line, into a slice */
-	lines := strings.Split(string(cont), "\n")
-
-	/* Remove the last element (empty) */
-	lines = lines[:len(lines)-1]
-
-	/* For every line in the CSV file, */
-	for _, l := range lines {
-
-		/* Split the fields based on the "," character */
-		fields := strings.Split(l, ",")
-
-		/* Extract the coordinates (/24, /32) */
-		x := strings.Split(fields[0], ".")[2]
-		y := strings.Split(fields[0], ".")[3]
-
-		/* Convert them to integers */
-		xint, err := strconv.Atoi(x)
-		if err != nil {
-			/*
-			 * If there's an error, exit. Feel free to modify code accordingly
-			 * to instead skip the line, using continue.
-			 */
-			slog.Error("failed to convert string to int", "error", err)
-			os.Exit(1)
-		}
-		yint, err := strconv.Atoi(y)
-		if err != nil {
-			slog.Error("failed to convert string to int", "error", err)
-			os.Exit(1)
+	inscan := bufio.NewScanner(cont)
+	for inscan.Scan() {
+		// Ignore comments
+		if strings.HasPrefix(inscan.Text(), CommentChar) {
+			continue
 		}
 
-		/*
-		 * Get the X, Y coordinates of the final PNG, by mapping the X and Y
-		 * using the Hilbert map created earlier
-		 */
-		actx, acty, err := hilb.Map(xint*256 + yint)
+		// Parse the line
+		rec, err := NewScanLine(inscan.Text())
 		if err != nil {
-			/* Exit on error, feel free to modify code as before */
-			slog.Error("failed to map point to hilbert curve", "error", err)
+			slog.Error("failed to parse output line", "error", err)
+			continue
+		}
+
+		// Ignore addresses not contained in the target range
+		if !prefix.Contains(rec.Address) {
+			continue
+		}
+
+		// Calculate the position within the range
+		foundAs4 := rec.Address.As4()
+		foundAsInt := big.NewInt(0).SetBytes(foundAs4[:])
+		index := foundAsInt.Sub(foundAsInt, rangeStart).Int64()
+
+		// Map to an X,Y in the hilbert curve
+		x, y, err := hilb.Map(int(index))
+		if err != nil {
+			slog.Error("failed to map address to hilbert curve", "line", inscan.Text(), "parsed", rec, "index", index, "error", err)
 			os.Exit(1)
 		}
 
-		/* Check the CSV field and determine if the pixel should be "on" */
-		if fields[1] == "up" {
-			/* If yes, color it green */
-			resultImage.Set(actx, acty, color.RGBA{50, 200, 50, 255})
-		} else {
-			/*
-			 * Otherwise, color it dark gray. It is not being colored as black,
-			 * because there will be no difference between missing elements
-			 * from the CSV, and pixels that are marked explicitly as "off".
-			 */
-			resultImage.Set(actx, acty, color.RGBA{50, 50, 50, 255})
+		// Set the image color
+		if rec.Status == open {
+			resultImage.Set(x, y, color.RGBA{50, 200, 50, 255})
 		}
+	}
 
+	if err := inscan.Err(); err != nil {
+		slog.Error("failed to scan input file", "error", err)
 	}
 
 	/* Write the PNG image to the output file */
---
+++ imager/parser.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/netip"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type ScanLine struct {
+	Status    ScanStatus
+	Protocol  string
+	Port      uint16
+	Address   netip.Addr
+	Timestamp time.Time
+}
+
+func NewScanLine(line string) (ScanLine, error) {
+	var ret ScanLine
+
+	fields := strings.Split(line, " ")
+
+	if len(fields) != 5 {
+		return ScanLine{}, fmt.Errorf("parsed fields are not 5")
+	}
+
+	// Status
+	if fields[0] == "open" {
+		ret.Status = open
+	} else {
+		ret.Status = closed
+	}
+
+	// Protocol
+	ret.Protocol = fields[1]
+
+	// Port
+	i, err := strconv.Atoi(fields[2])
+	if err != nil {
+		return ScanLine{}, err
+	}
+	ret.Port = uint16(i)
+
+	// Address
+	paddr, err := netip.ParseAddr(fields[3])
+	if err != nil {
+		return ScanLine{}, err
+	}
+	ret.Address = paddr
+
+	// Timestamp
+	tsp, err := strconv.Atoi(fields[4])
+	if err != nil {
+		return ScanLine{}, err
+	}
+	ret.Timestamp = time.Unix(int64(tsp), 0)
+
+	return ret, nil
+}
+
+type ScanStatus int
+
+const (
+	open ScanStatus = iota
+	closed
+)
